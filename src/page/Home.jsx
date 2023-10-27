8caeedca8aacb2b2fbf4367e61b8492807ec1aa5
sik2 <devpang20@gmail.com>
2023-10-27 21:30:35 +0900

    헤더 작업 1차

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,10 @@
+import Header from "../components/Header";
+
 function Home() {
     return ( 
         <>
-            메인 페이지
+            <Header />
+            <div>메인페이지내용</div>
         </> 
     );
 }
