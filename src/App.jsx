741415cbbef1badc8a23ea8a2df30593fcb786ba
sik2 <devpang20@gmail.com>
2023-11-10 20:37:33 +0900

    서브페이지 라우팅, 페이지 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./page/Home"
-import Movie from "./page/Moive"
+import MovieList from "./page/MoiveList"
 import Header from "./components/Header"
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="movie" element={<Movie />} />
+        <Route path="movies/:type" element={<MovieList />} />
       </Routes>
     </BrowserRouter>
   )
